974e79f10801e6fa025966768a2494fff79c80b7
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-06-08 10:55:31 +0200

    add tests, templates, static, instance folders, add env.example file

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 import os
 import sqlalchemy as sa
 
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+
 
 def create_app(config_type=None):
     app = Flask(__name__)
 
     if config_type == None:
-        config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
+        config_type = os.getenv(
+            'CONFIG_TYPE', default='config.DevelopmentConfig')
 
     app.config.from_object(config_type)
 
@@ -19,20 +23,30 @@ def create_app(config_type=None):
     register_blueprints(app)
 
     engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    inspector = sa.inspect(engine)
-    if not inspector.has_table("categories"):
-        with app.app_context():
-            db.drop_all()
-            db.create_all()
+    #inspector = sa.inspect(engine)
+    # if not inspector.has_table("categories"):
+    # with app.app_context():
+    # #     # print("ok")
+    # db.drop_all()
+    # db.create_all()
 
     return app
 
 
 def initialize_extensions(app):
     db.init_app(app)
-    from app.models.Category import Category
+    login_manager.init_app(app)
+
+    from app.models.User import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.filter(User.id == int(user_id)).first()
+
+    login_manager.login_view = "auth.login"
 
 
 def register_blueprints(app):
-    from .categories import categories
-    app.register_blueprint(categories, url_prefix="/categories")
+    from .auth import auth
+
+    app.register_blueprint(auth, url_prefix="/")
--- config.py
+++ config.py
@@ -1,27 +1,26 @@
 import os
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-DB_NAME = ""
-DB_USERNAME = "root"
-DB_PASSWORD = ""
-DB_HOST = "localhost"
-DB_PORT = 3306
+DB_NAME = os.getenv('DB_NAME')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+
 
 class Config(object):
     FLASK_ENV = 'development'
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.getenv('SECRET_KEY', default='BAD_SECRET_KEY')
-    
-    if os.getenv('DATABASE_URL'):
-        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    else:
-        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
-        # SQLALCHEMY_DATABASE_URI = f'mysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
+    SQLALCHEMY_DATABASE_URI = f'mysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-   
 
 
 class ProductionConfig(Config):
