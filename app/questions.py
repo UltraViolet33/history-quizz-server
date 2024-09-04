f12ad59ad5f080fb91e65361f673df86709322bf
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-09-04 16:17:26 +0200

    write quizzes to json file

--- app/questions.py
+++ app/questions.py
@@ -15,77 +15,87 @@ questions_folder = os.path.join(app_folder, "questions_files")
 
 @questions.route("/import-questions", methods=["GET", "POST"])
 def import_questions():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part")
             return redirect(request.url)
-        
-        file = request.files['file']
-        
-        if file.filename == '':
-            flash('No selected file')
+
+        file = request.files["file"]
+
+        if file.filename == "":
+            flash("No selected file")
             return redirect(request.url)
-        
+
         if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join(questions_folder, filename))
             import_questions_from_file(os.path.join(questions_folder, filename))
             # Further processing of the file can be done here
-            flash('File successfully uploaded')
-            return redirect(url_for('questions.import_questions'))
+            flash("File successfully uploaded")
+            return redirect(url_for("questions.import_questions"))
     return render_template("questions/import.html")
 
 
 @questions.route("/generate-json-file", methods=["GET"])
 def generate_json():
-    json_questions = []
-    questions = Question.query.all()
+    json_quizzes = []
+    quizzes = Quizz.query.all()
 
-    for question in questions:
-        question_dict = {
-            "id": question.id,
-            "text": question.text,
-            "answer1": question.right_answer.text,
-        }
-        i = 2
-        for answer in question.answers:
-            question_dict[f"answer{i}"] = answer.text
-            i += 1
-        json_questions.append(question_dict)
-        filename = os.path.join(questions_folder, 'questions.json')
+    for quizz in quizzes:
+        json_quizz = {"title": quizz.title, "questions": []}
 
-        with open(filename, 'w', encoding='utf-8') as f:
-            json.dump(json_questions, f, ensure_ascii=False, indent=4)
+        questions = quizz.questions
+        for question in questions:
+            question_dict = {"id": question.id, "text": question.text, "answers": []}
 
+            for answer in question.answers:
+                question_dict["answers"].append(
+                    {"id": answer.id, "text": answer.text, "isCorrect": False}
+                )
 
-    return json_questions
+            question_dict["answers"].append(
+                {
+                    "id": question.right_answer.id,
+                    "text": question.right_answer.text,
+                    "isCorrect": True,
+                }
+            )
+            json_quizz["questions"].append(question_dict)
+
+        json_quizzes.append(json_quizz)
+        filename = os.path.join(questions_folder, "questions.json")
+
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(json_quizzes, f, indent=4)
+
+    return json_quizzes
 
 
 def import_questions_from_file(filename):
-    with open(filename, encoding='utf-8') as file:
+    with open(filename, encoding="utf-8") as file:
         for line in file:
             print(line.rstrip())
-            if line == '\n':
+            if line == "\n":
                 continue
-            typet, text = line.split(':')
-            if typet == 'quizz':
+            typet, text = line.split(":")
+            if typet == "quizz":
                 quizz = Quizz.get_by_title(text)
                 if not quizz:
                     quizz = Quizz(title=text)
                     quizz.save()
-            if typet == 'question':
+            if typet == "question":
                 question_text = text
-            if typet == 'answer1':
+            if typet == "answer1":
                 right_answer = Answer(text)
                 right_answer.save()
-            if typet == 'answer2':
+            if typet == "answer2":
                 answer2 = Answer(text)
                 answer2.save()
-            if typet == 'answer3':
+            if typet == "answer3":
                 answer3 = Answer(text)
                 answer2.save()
 
-            if typet == 'answer4':
+            if typet == "answer4":
                 answer4 = Answer(text)
                 answer4.save()
                 question = Question(question_text, right_answer.id)
